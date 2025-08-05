84e884dfd2b4cbc3d8cf2b7c4b37746fb1374919
SuzukaHori <horikoshi.suzuka@gmail.com>
2025-08-05 20:03:58 +0900

    8/5分

---
+++ 3/3-2-4.rb
@@ -0,0 +1,49 @@
+class Gear
+  attr_reader :chainring, :cog, :rim, :tire
+
+  def initialize(chainring, cog, rim, tire)
+    @chainring = chainring
+    @cog = cog
+    @rim = rim
+    @tire = tire
+  end
+
+  def ratio
+    chainring / cog.to_f
+  end
+
+  def gear_inches
+    # 恐ろしい計算が何行かある
+    foo = some_intermediate_result * diameter
+    # 恐ろしい計算が何行かある
+  end
+
+  def diameter
+    wheel.diameter
+  end
+
+  def wheel
+    @wheel ||= Wheel.new(rim, tire)
+  end
+end
+
+
+class Wheel
+  attr_reader :rim, :tire
+
+  def initialize(rim, tire)
+    @rim = rim
+    @tire = tire
+  end
+
+  def diameter
+    # タイヤはリムの周りを囲むので、直径を計算するためには2倍する
+    rim + (tire * 2)
+  end
+
+  def circumference
+    diameter * Math::PI
+  end
+end
+
+puts Gear.new(52, 11, Wheel.new(26, 1.5)).gear_inches
