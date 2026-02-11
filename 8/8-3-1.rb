6e2417588e47fa838638a2c07a12ea2f0c9ebb0b
SuzukaHori <horikoshi.suzuka@gmail.com>
2026-02-11 22:10:07 +0900

    2/10分

---
+++ 8/8-3-1.rb
@@ -0,0 +1,48 @@
+class Parts
+  include Enumerable
+  attr_reader :parts
+
+  def initialize(parts)
+    @parts = parts #Array
+  end
+
+  def spares
+    parts.select { |part| part.needs_spare }
+  end
+end
+
+road_config = [
+  ["chain", "10-speed"],
+  ["tire_size", "23"],
+  ["tape_color", "red"]
+]
+
+mountain_config =  [
+  ["chain", "10-speed"],
+  ["tire_size", "2.1"],
+  ["front_shock", "Manitou", false],
+  ["rear_shock", "Fox"]
+]
+
+require "ostruct"
+
+module PartsFactory
+  def self.build(config, parts_class = Parts)
+    parts_class.new(
+      config.collect { |part_config|
+        create_part(part_config)
+      }
+    )
+  end
+
+  def self.create_part(part_config)
+    OpenStruct.new(
+      name: part_config[0],
+      description: part_config[1],
+      needs_spare: part_config.fetch(2, true)
+    )
+  end
+end
+
+p road_parts = PartsFactory.build(road_config)
+p mountain_parts = PartsFactory.build(mountain_config)
